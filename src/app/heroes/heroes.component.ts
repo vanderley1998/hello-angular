ba2819396e3d1d56748b721c0b4d54823dcba029
Vanderley Sousa <vanderley_1998@hotmail.com.br>
2019-02-22 14:52:48 -0300

    "Master/Detail Components" is done and started to do "services"

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +9,17 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = Hero[];
 
-  constructor() { }
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
